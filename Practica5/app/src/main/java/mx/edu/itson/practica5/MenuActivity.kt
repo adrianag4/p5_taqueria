252c53c18320c35b66480cc8eb276fc38985e0b6
adriana <adriana.gutierrez235633@potros.itson.edu.mx>
2025-02-26 20:25:41 -0700

    pt 2

--- Practica5/app/src/main/java/mx/edu/itson/practica5/MenuActivity.kt
+++ Practica5/app/src/main/java/mx/edu/itson/practica5/MenuActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.practica5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,48 @@ class MenuActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var btnAntojitos: Button = findViewById(R.id.btnAntojitos) as Button
+        var btnTortas: Button = findViewById(R.id.btnTortas) as Button
+        var btnEspecialidades: Button = findViewById(R.id.btnEspecialidades) as Button
+        var btnCombos: Button = findViewById(R.id.btnCombinations) as Button
+        var btnSopas: Button = findViewById(R.id.btnSopas) as Button
+        var btnBebidas: Button = findViewById(R.id.btnDinks) as Button
+
+        btnAntojitos.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "ANTOJITOS")
+            startActivity(intent)
+        }
+
+        btnTortas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "TORTAS")
+            startActivity(intent)
+        }
+
+        btnEspecialidades.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "ESPECIALIDADES")
+            startActivity(intent)
+        }
+
+        btnCombos.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "COMBOS")
+            startActivity(intent)
+        }
+
+        btnSopas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "SOPAS")
+            startActivity(intent)
+        }
+
+        btnBebidas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "BEBIDAS")
+            startActivity(intent)
         }
     }
 }
--- Practica5/app/src/main/java/mx/edu/itson/practica5/RegisterActivity.kt
+++ Practica5/app/src/main/java/mx/edu/itson/practica5/RegisterActivity.kt
@@ -3,17 +3,26 @@ package mx.edu.itson.practica5
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        val button: Button = findViewById(R.id.sing_in_button)
+        val btn: Button = findViewById(R.id.btnSingIn)
 
-        button.setOnClickListener{
-            var intent= Intent(this,MenuActivity::class.java)
+        btn.setOnClickListener{
+            var intent: Intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
     }
